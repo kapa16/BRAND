92475c9dd3ea9de7cffcbdbbbe8ffbc2ca181db3
Pavel <kapa16@inbox.ru>
2018-10-26 13:16:34 +0500

    settings gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,130 @@
-var gulp = require('gulp'),
-    sass = require('gulp-sass');
+"use strict";
+/* пути к исходным файлам (src), к готовым файлам (build), а также к тем, за изменениями которых нужно наблюдать (watch) */
+var path = {
+    build: {
+        html:  'app/',
+        js:    'app/js/',
+        css:   'app/css/',
+        img:   'app/img/',
+        fonts: 'app/fonts/'
+    },
+    src: {
+        html:  'dist/*.html',
+        js:    'dist/js/main.js',
+        style: 'dist/style/main.scss',
+        img:   'dist/img/**/*.*',
+        fonts: 'dist/fonts/**/*.*'
+    },
+    watch: {
+        html:  'dist/**/*.html',
+        js:    'dist/js/**/*.js',
+        css:   'dist/style/**/*.scss',
+        img:   'dist/img/**/*.*',
+        fonts: 'dist/fonts/**/*.*'
+    },
+    clean:     './app'
+};
+/* настройки сервера */
+var config = {
+    server: {
+        baseDir: './app'
+    },
+    notify: false
+};
 
-gulp.task('sass', function () {
-    return gulp.src('dist/scss/style.scss')
-        .pipe(sass())
-        .pipe(gulp.dest('app/css'))
+/* подключаем gulp и плагины */
+var gulp = require('gulp'),  // подключаем Gulp
+    webserver = require('browser-sync'), // сервер для работы и автоматического обновления страниц
+    plumber = require('gulp-plumber'), // модуль для отслеживания ошибок
+    rigger = require('gulp-rigger'), // модуль для импорта содержимого одного файла в другой
+    sourcemaps = require('gulp-sourcemaps'), // модуль для генерации карты исходных файлов
+    sass = require('gulp-sass'), // модуль для компиляции SASS (SCSS) в CSS
+    cache = require('gulp-cache'), // модуль для кэширования
+    del = require('del'); // плагин для удаления файлов и каталогов
+
+/* задачи */
+
+// запуск сервера
+gulp.task('webserver', function () {
+    webserver(config);
 });
+
+// сбор html
+gulp.task('html:build', function () {
+    gulp.src(path.src.html) // выбор всех html файлов по указанному пути
+        .pipe(plumber()) // отслеживание ошибок
+        .pipe(rigger()) // импорт вложений
+        .pipe(gulp.dest(path.build.html)) // выкладывание готовых файлов
+        .pipe(webserver.reload({stream: true})); // перезагрузка сервера
+});
+
+// сбор стилей
+gulp.task('css:build', function () {
+    gulp.src(path.src.style) // получим main.scss
+        .pipe(plumber()) // для отслеживания ошибок
+        .pipe(sourcemaps.init()) // инициализируем sourcemap
+        .pipe(sass()) // scss -> css
+        .pipe(sourcemaps.write('./')) // записываем sourcemap
+        .pipe(gulp.dest(path.build.css)) // выгружаем в build
+        .pipe(webserver.reload({stream: true})); // перезагрузим сервер
+});
+
+// сбор js
+gulp.task('js:build', function () {
+    gulp.src(path.src.js) // получим файл main.js
+        .pipe(plumber()) // для отслеживания ошибок
+        .pipe(rigger()) // импортируем все указанные файлы в main.js
+        .pipe(sourcemaps.init()) //инициализируем sourcemap
+        .pipe(sourcemaps.write('./')) //  записываем sourcemap
+        .pipe(gulp.dest(path.build.js)) // положим готовый файл
+        .pipe(webserver.reload({stream: true})); // перезагрузим сервер
+});
+
+// перенос шрифтов
+gulp.task('fonts:build', function() {
+    gulp.src(path.src.fonts)
+        .pipe(gulp.dest(path.build.fonts));
+});
+
+// обработка картинок
+gulp.task('image:build', function () {
+    gulp.src(path.src.img) // путь с исходниками картинок
+        .pipe(gulp.dest(path.build.img)); // выгрузка готовых файлов
+});
+
+// удаление каталога build 
+gulp.task('clean:build', function () {
+    del.sync(path.clean);
+});
+
+// очистка кэша
+gulp.task('cache:clear', function () {
+  cache.clearAll();
+});
+
+// сборка
+gulp.task('build', [
+    'clean:build',
+    'html:build',
+    'css:build',
+    'js:build',
+    'fonts:build',
+    'image:build'
+]);
+
+// запуск задач при изменении файлов
+gulp.task('watch', function() {
+    gulp.watch(path.watch.html, ['html:build']);
+    gulp.watch(path.watch.css, ['css:build']);
+    gulp.watch(path.watch.js, ['js:build']);
+    gulp.watch(path.watch.img, ['image:build']);
+    gulp.watch(path.watch.fonts, ['fonts:build']);
+});
+
+// задача по умолчанию
+gulp.task('default', [
+    'clean:build',
+    'build',
+    'webserver',
+    'watch'
+]);
