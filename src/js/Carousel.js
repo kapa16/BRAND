7b1d1dc681ef4956679d957257b238dc6e1f7c66
kapa <kapa16@inbox.ru>
2019-02-09 20:45:32 +0500

    refactor code, which change visible element

--- src/js/Carousel.js
+++ src/js/Carousel.js
@@ -39,24 +39,36 @@ class Carousel {
 
   /**
    * Меняет изображение в переданном направлении
-   * @param {integer} direction - направление смены изображения
+   * @param {Number} direction - направление смены изображения
    * @private
    */
   _changeSlide(direction) {
     const $currentElement = $('.carousel__block:visible');
-    let $nextElement = $currentElement;
-    if (direction === 1) {
-      $nextElement = $currentElement.next('.carousel__block');
-      if (!$nextElement.length) {
-        $nextElement = $('.carousel__block').first();
-      }
-    } else {
-      $nextElement = $currentElement.prev('.carousel__block');
-      if (!$nextElement.length) {
-        $nextElement = $('.carousel__block').last();
-      }
-    }
-    $nextElement.show();
+    const $carouselElements = $('.carousel__block');
+
+    const currentIndex = $currentElement.index();
+    const lastElementIndex = $carouselElements.length - 1;
+    const nextIndex = this._getNextElementIndex(direction, currentIndex, lastElementIndex);
+
     $currentElement.hide();
+    $carouselElements.eq(nextIndex).show();
+  }
+
+  /**
+   * Получает индекс следующего элемента карусели, исходя из направления смены элементов
+   * @param {Number} direction - направление смены
+   * @param {Number} currentIndex - индекс текущего видимого элемента
+   * @param {Number} lastElementIndex - индекс последнего элемента в коллекции
+   * @returns {Number} - индекс следующего элемента
+   * @private
+   */
+  _getNextElementIndex(direction, currentIndex, lastElementIndex) {
+    let nextIndex = currentIndex + direction;
+    if (nextIndex < 0) {
+      nextIndex = lastElementIndex;
+    } else if (nextIndex > lastElementIndex) {
+      nextIndex = 0;
+    }
+    return nextIndex;
   }
 }
